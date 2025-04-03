b4a00b08cb7f934e6f126751116c8ab480b8daca
ANAMASGARD <chaudharygaurav2004@gmail.com>
2025-04-03 15:45:43 +0530

    Register page added for Registeration

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
+
+
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO : OTP VERIFICATION | PASSKEYMODAL*/}
      <section className="remove-scrolbar container my-auto">
       <div className="sub-container max-w-[496px]">
       <Image
---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,92 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation";
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form,} from "@/components/ui/form"
+
+import { createUser } from "@/lib/actions/patient.actions";
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation"
+
+import { FormFieldType } from "./PatientForm";
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+ 
+const RegisterForm = ({user} : {user : User }) => {
+  // 1. Define your form.
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+        setIsLoading(true);
+    
+        try {
+          const user = {
+            name: values.name,
+            email: values.email,
+            phone: values.phone,
+          };
+    
+          const newUser = await createUser(user);
+    
+          if (newUser) {
+            router.push(`/patients/${newUser.$id}/register`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+    
+        setIsLoading(false);
+      };
+  return (
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flx-1">
+        <section className="mb-6  spcae-y-4 flex items-center justify-between">
+        
+          <p className="text-dark-700">Get started with appointments 🗓️.</p>
+        </section>
+
+        
+
+       <CustomFormField 
+               fieldType={FormFieldType.INPUT}
+               control={form.control}
+               name="email"
+               label="Full name"
+               placeholder="gauravchaudhary@gmail.com"
+               iconSrc="/assets/icons/email.svg"
+               iconAlt="email"
+               
+               />
+       
+      
+       <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+    </form>
+  </Form>
+  )
+}
+
+export default RegisterForm
